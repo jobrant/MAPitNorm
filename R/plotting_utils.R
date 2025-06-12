9668c4104309cd18f6c1bdd629c6dca58ba5c118
Jason Orr Brant <jason.o.brant@gmail.com>
2025-06-12 17:02:29 -0400

    update comments only

--- R/plotting_utils.R
+++ R/plotting_utils.R
@@ -1,13 +1,16 @@
 #' @title Internal Visualization Utility Functions
 #' @description This file contains internal utility functions for visualization.
+#' @name visualization-utils
 #' @importFrom grDevices pdf dev.off
 #' @importFrom grid grid.newpage grid.text gpar
 #' @importFrom utils combn
 #' @importFrom stats aggregate na.omit
 #' @importFrom reshape2 dcast
 #' @importFrom patchwork wrap_plots
-#' @importFrom ggplot2 ggplot aes aes_string geom_point geom_abline geom_density geom_hex geom_bar facet_wrap labs theme theme_minimal element_text
-#' @importFrom viridis scale_fill_viridis_c
+#' @importFrom ggplot2 ggplot aes aes_string geom_point geom_abline
+#' geom_density geom_hex geom_bar facet_wrap labs theme theme_minimal
+#' element_text scale_fill_viridis_c
+#' @importFrom viridisLite viridis
 NULL
 
 
