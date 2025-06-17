b801dac85124f767566f3b5b311db441ac262f9e
Jason Orr Brant <jason.o.brant@gmail.com>
2025-06-17 14:49:26 -0400

    Add extern c to Rcpp export.

--- src/read_methylation.cpp
+++ src/read_methylation.cpp
@@ -22,68 +22,75 @@ std::vector<std::string> readGzippedLines(const char* filename) {
   return lines;
 }
 
-// [[Rcpp::export]]
-DataFrame readMethylationFile(std::string filename) {
-  // Read all lines from the gzipped file
-  std::vector<std::string> lines = readGzippedLines(filename.c_str());
-  
-  // Preallocate vectors for data
-  int n_lines = lines.size();
-  CharacterVector chr(n_lines);
-  IntegerVector pos(n_lines);
-  CharacterVector strand(n_lines);
-  CharacterVector site(n_lines);
-  IntegerVector mc(n_lines);
-  IntegerVector cov(n_lines);
-  
-  // Parse each line
-  for (int i = 0; i < n_lines; i++) {
-    std::string line = lines[i];
-    std::istringstream iss(line);
-    std::string chr_val, strand_val, site_val;
-    int pos_val, mc_val, cov_val;
+extern "C" {
+  // [[Rcpp::export]]
+  DataFrame readMethylationFile(std::string filename) {
+    Rcout << "Reading file: " << filename << "\n";
+    // Read all lines from the gzipped file
+    std::vector<std::string> lines = readGzippedLines(filename.c_str());
     
-    // Read tab-separated values
-    if (std::getline(iss, chr_val, '\t') && 
-        (iss >> pos_val) && iss.ignore() &&
-        std::getline(iss, strand_val, '\t') &&
-        std::getline(iss, site_val, '\t') &&
-        (iss >> mc_val) && iss.ignore() &&
-        (iss >> cov_val)) {
-        
-      // Store in vectors
-      chr[i] = chr_val;
-      pos[i] = pos_val;
-      strand[i] = strand_val;
-      site[i] = site_val;
-      mc[i] = mc_val;
-      cov[i] = cov_val;
+    // Preallocate vectors for data
+    int n_lines = lines.size();
+    CharacterVector chr(n_lines);
+    IntegerVector pos(n_lines);
+    CharacterVector strand(n_lines);
+    CharacterVector site(n_lines);
+    IntegerVector mc(n_lines);
+    IntegerVector cov(n_lines);
+    
+    // Parse each line
+    for (int i = 0; i < n_lines; i++) {
+      std::string line = lines[i];
+      std::istringstream iss(line);
+      std::string chr_val, strand_val, site_val;
+      int pos_val, mc_val, cov_val;
+      
+      // Read tab-separated values
+      if (std::getline(iss, chr_val, '\t') && 
+          (iss >> pos_val) && iss.ignore() &&
+          std::getline(iss, strand_val, '\t') &&
+          std::getline(iss, site_val, '\t') &&
+          (iss >> mc_val) && iss.ignore() &&
+          (iss >> cov_val)) {
+          
+        // Store in vectors
+        chr[i] = chr_val;
+        pos[i] = pos_val;
+        strand[i] = strand_val;
+        site[i] = site_val;
+        mc[i] = mc_val;
+        cov[i] = cov_val;
+      }
     }
+    
+    // Create and return data frame
+    DataFrame df = DataFrame::create(
+      _["chr"] = chr,
+      _["pos"] = pos,
+      _["strand"] = strand,
+      _["site"] = site,
+      _["mc"] = mc,
+      _["cov"] = cov
+    );
+    
+    return df;
   }
-  
-  // Create and return data frame
-  DataFrame df = DataFrame::create(
-    _["chr"] = chr,
-    _["pos"] = pos,
-    _["strand"] = strand,
-    _["site"] = site,
-    _["mc"] = mc,
-    _["cov"] = cov
-  );
-  
-  return df;
-}
 
-// [[Rcpp::export]]
-List readMethylationFiles(CharacterVector filenames) {
-  int n_files = filenames.size();
-  List results(n_files);
-  
-  for (int i = 0; i < n_files; i++) {
-    std::string filename = as<std::string>(filenames[i]);
-    Rcpp::checkUserInterrupt(); // Allow user to cancel
-    results[i] = readMethylationFile(filename);
-  }
-  
-  return results;
+
+  // [[Rcpp::export]]
+  List readMethylationFiles(CharacterVector filenames) {
+    Rcout << "Entering readMethylationFiles with " << filenames.size() << " files\n";
+    int n_files = filenames.size();
+    List results(n_files);
+    
+    for (int i = 0; i < n_files; i++) {
+      std::string filename = as<std::string>(filenames[i]);
+      Rcout << "Processing file: " << filename << "\n";
+      Rcpp::checkUserInterrupt(); // Allow user to cancel
+      results[i] = readMethylationFile(filename);
+    }
+    
+    Rcout << "Exiting readMethylationFiles\n";
+    return results;
+  } 
 }
