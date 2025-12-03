f8ab402b3c92874a5b3e20ee8ecf02edc0e49b9f
heatherkates <hkates@ufl.edu>
2025-12-03 14:58:29 -0500

    make args consistent with create_bedgraph and add import funcs

--- R/create_BigWig.R
+++ R/create_BigWig.R
@@ -6,15 +6,16 @@
 #'
 #' @param data List object returned by \code{\link{load_data}} containing
 #'   methylation data with sample_metadata attribute
-#' @param output_dir Directory path for output BigWig files
+#' @param out Directory path for output BigWig files
 #' @param genome Genome build. One of "hg38", "hg19", "mm10", "mm39". Default is "hg38"
 #' @param sample_name Sample ID to export (for single sample export)
 #' @param aggregate_replicates Logical. If TRUE, aggregates replicates within a group.
 #'   Default is FALSE
-#' @param group_id Group ID to aggregate (required if aggregate_replicates = TRUE)
+#' @param group_name Group ID to aggregate (required if aggregate_replicates = TRUE)
 #' @param min_coverage Minimum coverage threshold for including sites. Default is 1
-#' @param split_strand Logical. If TRUE, creates separate BigWig files for plus
-#'   and minus strands. Default is FALSE
+#' @param type Character string for type of value to include as score in bigwig.
+#'        Should be one of "cov" or "meth" (for coverage score or methylation rate score, respectively.)
+
 #'
 #' @return Character vector of created file path(s)
 #'
@@ -31,36 +32,42 @@
 #' )
 #'
 #' # Export single sample
-#' export_to_bigwig(
+#' create_bigwig(
 #'   data = all_samples,
-#'   output_dir = "bigwigs",
-#'   sample_name = "allc_M1N2"
+#'   out = "bigwigs",
+#'   sample_name = "allc_M1N2",
+#'   type = "meth"
 #' )
 #'
 #' # Export aggregated replicates
-#' export_to_bigwig(
+#' create_bigwig(
 #'   data = all_samples,
-#'   output_dir = "bigwigs",
+#'   out = "bigwigs",
 #'   aggregate_replicates = TRUE,
-#'   group_id = "M1"
+#'   group_name = "M1",
+#'   type = "rate"
 #' )
 #' }
 #'
-#' @importFrom rtracklayer export.bw
-#' @importFrom GenomicRanges GRanges makeGRangesFromDataFrame
+#' @importFrom GenomeInfoDb seqinfo seqinfo<- seqlevels keepSeqlevels
+#' @importFrom GenomicRanges GRanges
 #' @importFrom IRanges IRanges
-#' @importFrom GenomeInfoDb seqinfo seqlevels keepSeqlevels
+#' @importFrom rtracklayer export.bw import.bw
 #' @importFrom data.table rbindlist copy
 #' @export
-export_to_bigwig <- function(
+create_bigwig <- function(
     data,
-    output_dir,
-    genome = "hg38",
+    type=c("meth","cov"),
+    out,
+    group_name = NULL,
     sample_name = NULL,
+    genome = "hg38",
     aggregate_replicates = FALSE,
-    group_id = NULL,
     min_coverage = 1)
+
   {
+  # Match and check type argument
+  type <- match.arg(type)
   # Only check for the genome-specific package (in Suggests)
   genome_pkg <- switch(genome,
                        "hg38" = "BSgenome.Hsapiens.UCSC.hg38",
@@ -74,7 +81,8 @@ export_to_bigwig <- function(
     stop(paste0("Package ", genome_pkg, " not installed. Install with: BiocManager::install('", genome_pkg, "')"))
   }
 
-  genome_obj <- get(genome_pkg)
+  # Access the BSgenome object from the package namespace
+  genome_obj <- getExportedValue(genome_pkg, genome_pkg)
   genome_info <- seqinfo(genome_obj)
 
     # Get metadata from data object
@@ -85,11 +93,9 @@ export_to_bigwig <- function(
 
     # Get the data to export
     if (aggregate_replicates) {
-      if (is.null(group_id)) stop("group_id required for aggregation")
+      if (is.null(group_name)) stop("group_name required for aggregation")
 
-      # Use intermediate variable to avoid column name confusion
-      target_group <- group_id
-      samples_to_agg <- metadata[group_id == target_group, sample_id]
+      samples_to_agg <- metadata[group_id == group_name, sample_id]
       dt_list <- data[samples_to_agg]
       combined <- rbindlist(dt_list)
 
@@ -99,7 +105,7 @@ export_to_bigwig <- function(
         cov = sum(cov)
       ), by = .(chr, pos, strand, site)]
 
-      file_base <- paste0(target_group, "_aggregated")
+      file_base <- paste0(group_name, "_aggregated")
     }
     else {
       if (is.null(sample_name)) stop("sample_name required for single sample export")
@@ -112,11 +118,13 @@ export_to_bigwig <- function(
 
     # Export function
     export <- function(dt, strand_label = "") {
+      # Map type to actual column name
+      score_col <- if(type == "meth") "rate" else "cov"
       gr <- GRanges(
         seqnames = dt$chr,
         ranges = IRanges(start = dt$pos, width = 1),
         strand = dt$strand,
-        score = dt$rate
+        score = dt[[score_col]]  # Use the selected type column
       )
 
       # Only keep chromosomes that exist in genome_info
@@ -124,8 +132,8 @@ export_to_bigwig <- function(
                           pruning.mode = "coarse")
       seqinfo(gr) <- genome_info[seqlevels(gr)]
 
-      out_file <- file.path(output_dir,
-                            paste0(file_base, strand_label, ".bw"))
+      out_file <- file.path(out,
+                            paste0(file_base, strand_label, "_", type, ".bw"))
       export.bw(gr, out_file)
       return(out_file)
     }
